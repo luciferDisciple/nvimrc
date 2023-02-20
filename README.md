56049ec4350cd481395d8081228eb0eb2545e58a
luciferDisciple <waizer12@gmail.com>
2023-02-20 22:54:53 +0100

    fixed install script

---
+++ README.md
@@ -0,0 +1,12 @@
+# Lucifer Disciple's nvimrc
+
+## Installation
+
+```
+$ ./install.sh
+Do you want to make backup and continue? [y/N] Y
+Backed up '/home/piotr/.config/nvim/' to '/home/piotr/.config/nvim.bak.hAY/'.
+Installing...
+Success.
+$
+```
--- install.sh
+++ install.sh
@@ -4,7 +4,8 @@ set -e
 
 main() {
 	backup
-	[[ ! -f ~/.config/nvim/ ]] && mkdir ~/.config/nvim/
+	echo Installing...
+	[[ -f ~/.config/nvim/ ]] || mkdir ~/.config/nvim/
 	cp init.lua ~/.config/nvim/
 	echo Success.
 }
@@ -15,16 +16,18 @@ dir_is_not_empty() {
 }
 
 backup() {
-	if [[ -f ~/.config/nvim && dir_is_not_empty ~/.config/nvim ]]; then
+	if [[ -e ~/.config/nvim/ ]] && dir_is_not_empty ~/.config/nvim ; then
 		echo "Directory '$HOME/.config/nvim' already exists."
 		while :; do
 			read -p  "Do you want to make backup and continue? [y/N] "
-			if [[ -z "$REPLY" || "$REPLY" = y || "$REPLY" = Y ]]; then
+			if [[ "$REPLY" =~ [yY] ]]; then
 				tempdir="$(mktemp -d ~/.config/nvim.bak.XXX)"
 				rmdir "$tempdir"
-				mv ~/.config/nvim "$tempdir"
+				mv ~/.config/nvim/ "$tempdir"
 				echo "Backed up '$HOME/.config/nvim/' to '$tempdir/'."
-			else
+				break
+			elif [[ "$REPLY" =~ [nN] || -z "$REPLY" ]]; then
+				echo Aborting.
 				exit 1
 			fi
 		done

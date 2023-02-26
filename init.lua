c9e5057f4c5a9c0761e48550417a5a8cc164ba03
luciferDisciple <waizer12@gmail.com>
2023-02-26 14:33:40 +0100

    insert mode: quit with 'jj'

--- init.lua
+++ init.lua
@@ -240,6 +240,8 @@ vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 
+vim.keymap.set('i', 'jj', '<Esc>')
+
 -- [[ Highlight on yank ]]
 -- See `:help vim.highlight.on_yank()`
 local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })

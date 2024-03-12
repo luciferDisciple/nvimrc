cd8dfa81a5031bc6129da6f4d0f6fe7af7051aba
luciferDisciple <waizer12@gmail.com>
2024-03-12 23:15:58 +0100

    refactor: main(), consistent variable naming
    1. Moved top-level commands into main() function.
    2. Uppercase-only names reserved to environment variables. Script's
       variable names are lowercase. Cryptic names like "DIR" replaced
       with more descriptive names like "this_script_dir".

--- install.sh
+++ install.sh
@@ -1,8 +1,26 @@
 #!/bin/bash
 shopt -s nocasematch
-DIR="$(dirname "$(realpath "$0")")"  # directory of this script
-vimrc_destination="$HOME/.config/nvim/init.vim"
-vimrc_source="$DIR/init.vim"
+
+this_script_dir="$(dirname "$(realpath "$0")")"
+vimrc_destination_dir="$HOME/.config/nvim"
+vimrc_destination="$vimrc_destination_dir/init.vim"
+vimrc_source="$this_script_dir/init.vim"
+
+main() {
+	if [ -f "$vimrc_destination" ]; then
+		echo File "'$vimrc_destination'" already exists. It will be backed up.
+		read -p "Do you want to proceed [Y/n]? "
+		if ! [[ "$REPLY" =~ ^(y|yes|)$ ]]; then
+			echo Aborting...
+			exit 1
+		fi
+		backup_vimrc
+		rm "$vimrc_destination"
+	fi
+	echo Creating symoblic link "'$vimrc_destination'" to "'$vimrc_source'"
+	ln -s "$vimrc_source" "$vimrc_destination"
+	echo Finished!
+}
 
 backup_vimrc() {
 	backups_count=1
@@ -15,16 +33,4 @@ backup_vimrc() {
 	cp "$vimrc_destination" "$backup"
 }
 
-if [ -f "$vimrc_destination" ]; then
-	echo File "'$vimrc_destination'" already exists. It will be backed up.
-	read -p "Do you want to proceed [Y/n]? "
-	if ! [[ "$REPLY" =~ ^(y|yes|)$ ]]; then
-		echo Aborting...
-		exit 1
-	fi
-	backup_vimrc
-	rm "$vimrc_destination"
-fi
-echo Creating symoblic link "'$vimrc_destination'" to "'$vimrc_source'"
-ln -s "$vimrc_source" "$vimrc_destination"
-echo Finished!
+main
